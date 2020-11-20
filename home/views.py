0ad61a3b9a4fcfa6d5b8560455b638a2dcf1b0f1
Deepak Babani <72084232+Deepakbabani@users.noreply.github.com>
2020-11-20 14:43:45 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -4,15 +4,16 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    return render(request, 'index.html')
 def payment(request):
-    if request.method=="GET":
-        amount = request.GET.get("amount")
-        email = request.GET.get("email")
-        content = f"Payment recieved of {amount} recieved"
-        send_mail("Payment Gateway Integration", content, "paymentnone8@gmail.com", [email])
-        context = {
-            "amount":amount
-        }
-    return render(request,"payment.html",context)
+    if request.method == "GET":
+        amount = request.GET.get('amount')
+        email = request.GET.get('email')
+        Email = request.GET.get('Email')
+        content = f"Payment recieved. Thanks for this donation."
+        if email is None:
+            send_mail("Payment Gateaway Integration", content, "paymentnone8@gmail.com", [Email])
+        elif Email is None:
+            send_mail("Payment Gateaway Integration", content, "paymentnone8@gmail.com", [email])
+    return render(request, 'payment.html')
     
